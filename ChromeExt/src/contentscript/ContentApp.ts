lupuslabs/n3qExt

commit 154896b0d7e0e219efa487a495e0fae98a51a730

Item rezzing/derezzing usability improvements
- Implemented item (de)rezzing by CTRL-click on a Backpackitem or RoomItem.
- Derezzing by drag into backpack now also changes item position in backpack.
- Centralized item derezzing on the content side.

ChromeExt/src/contentscript/ContentApp.ts
@@ -23,11 +23,12 @@ import { SettingsWindow } from './SettingsWindow';
 import { XmppWindow } from './XmppWindow';
 import { ChangesWindow } from './ChangesWindow';
 import { BackpackWindow } from './BackpackWindow';
-import { SimpleToast } from './Toast';
+import { ItemExceptionToast, SimpleToast } from './Toast';
 import { IframeApi } from './IframeApi';
 import { RandomNames } from '../lib/RandomNames';
 import { Participant } from './Participant';
 import { SimpleItemTransferController } from './SimpleItemTransferController';
+import { ItemException } from '../lib/ItemException';
 
 interface ILocationMapperResponse
 {
@@ -805,6 +806,20 @@ export class ContentApp
         Panic.now();
     }
 
+    public onItemError(
+        src: string,
+        msg: string,
+        error: unknown,
+        ...data: unknown[]
+    ): void {
+        if (error instanceof ItemException) {
+            const duration = Config.getNumber('room.errorToastDurationSec');
+            new ItemExceptionToast(this, duration, error).show();
+        } else {
+            this.onError(src, msg, error, ...data);
+        }
+    }
+
     // Window management
 
     public static LayerBelowEntities = 20;
@@ -1029,4 +1044,57 @@ export class ContentApp
         }
         return pos;
     }
+
+    // Item helpers
+
+    /**
+     * Triggers the derezzing of the item with
+     * or without setting a new backpack position.
+     */
+    public derezItem(
+        itemId: string,
+        xNew?: undefined|number,
+        yNew?: undefined|number,
+    ): void {
+        const roomItem = this.room.getItem(itemId);
+        if (!is.nil(roomItem)) {
+            roomItem.beginDerez();
+        }
+        this.derezItemAsync(itemId, xNew, yNew
+        ).catch(error => {this.onItemError(
+            'ContentApp.derezItem',
+            'ContentApp.derezItemAsync failed!',
+            error, 'itemId', itemId, 'xNew', xNew, 'yNew', yNew
+        )}).finally(() => {
+            const roomItem = this.room.getItem(itemId);
+            if (!is.nil(roomItem)) {
+                roomItem.endDerez();
+            }
+        });
+    }
+
+    /**
+     * Derezzes the item with or without setting a new backpack position.
+     *
+     * Async version allowing direct reaction to errors.
+     */
+    public async derezItemAsync(
+        itemId: string,
+        xNew?: undefined|number,
+        yNew?: undefined|number,
+    ): Promise<void> {
+        const props = await BackgroundMessage.getBackpackItemProperties(itemId);
+        const roomJid = props[Pid.RezzedLocation];
+        const [x, y] = [xNew ?? -1, yNew ?? -1];
+        const propsDel = [Pid.AutorezIsActive, Pid.State];
+        if (Utils.logChannel('items')) {
+            log.info(
+                'BackpackWindow.derezItem',
+                'itemId', itemId, 'roomJid', roomJid);
+        }
+        await BackgroundMessage.derezBackpackItem(
+            itemId, roomJid, x, y, {}, propsDel, {}
+        );
+    }
+
 }

ChromeExt/src/contentscript/BackpackItem.ts
@@ -54,26 +54,8 @@ export class BackpackItem
         $(paneElem).append(this.elem);
 
         $(this.elem).on({
-            mousedown: (ev) =>
-            {
-                this.mousedownX = ev.clientX;
-                this.mousedownY = ev.clientY;
-            },
-            click: (ev) => 
-            {
-                if (Math.abs(this.mousedownX - ev.clientX) > 2 || Math.abs(this.mousedownY - ev.clientY) > 2) {
-                    return;
-                }
-
-                this.app.toFront(this.getElem(), ContentApp.LayerWindowContent);
-                if (this.info) {
-                    this.info?.close();
-                } else {
-                    this.info = new BackpackItemInfo(this.app, this, () => { this.info = null; });
-                    this.info.show(ev.offsetX, ev.offsetY);
-                    this.app.toFront(this.info.getElem(), ContentApp.LayerWindowContent);
-                }
-            }
+            mousedown: this.onMouseDown.bind(this),
+            click: this.onMouseClick.bind(this),
         });
 
         $(this.elem).draggable({
@@ -187,9 +169,40 @@ export class BackpackItem
         this.ignoreNextDropFlag = true;
     }
 
-    onMouseClick(ev: JQuery.Event): void
+    private onMouseDown(ev: JQuery.MouseDownEvent): void
     {
-        this.app.toFront(this.elem, ContentApp.LayerWindowContent);
+        this.mousedownX = ev.clientX;
+        this.mousedownY = ev.clientY;
+    }
+
+    private onMouseClick(ev: JQuery.ClickEvent): void
+    {
+        if (Math.abs(this.mousedownX - ev.clientX) > 2
+        || Math.abs(this.mousedownY - ev.clientY) > 2) {
+            return;
+        }
+        this.app.toFront(this.getElem(), ContentApp.LayerWindowContent);
+        const infoOpen = this.info;
+        if (infoOpen) {
+            this.info?.close();
+        }
+        if (!ev.shiftKey && !ev.ctrlKey && !ev.altKey && !ev.metaKey) {
+            // Just a click.
+            if (!infoOpen) {
+                const onClose = () => { this.info = null; };
+                this.info = new BackpackItemInfo(this.app, this, onClose);
+                this.info.show(ev.offsetX, ev.offsetY);
+                this.app.toFront(this.info.getElem(), ContentApp.LayerWindowContent);
+            }
+        } else if (!ev.shiftKey && ev.ctrlKey && !ev.altKey && !ev.metaKey) {
+            // CTRL + click.
+            if (as.Bool(this.properties[Pid.IsRezzed], false)) {
+                this.app.derezItem(this.getItemId());
+            } else {
+                const rezzXProp = this.properties[Pid.RezzedX];
+                this.rezItem(as.Int(rezzXProp, ev.clientX));
+            }
+        }
     }
 
     private dragIsRezable: boolean = false;
@@ -339,11 +352,6 @@ export class BackpackItem
     rezItem(x: number)
     {
         this.backpackWindow.rezItemSync(this.itemId, this.app.getRoom().getJid(), Math.round(x), this.app.getRoom().getDestination());
-    }
-
-    derezItem()
-    {
-        this.backpackWindow.derezItem(this.itemId, this.properties[Pid.RezzedLocation], -1, -1);
     }
 
     getPseudoRandomCoordinate(space: number, size: number, padding: number, id: string, mod: number): number

ChromeExt/src/contentscript/BackpackItemInfo.ts
@@ -145,7 +145,7 @@ export class BackpackItemInfo
             $(derezElem).on('click', (ev) =>
             {
                 ev.stopPropagation();
-                this.backpackItem.derezItem();
+                this.app.derezItem(this.backpackItem.getItemId());
                 this.close();
             });
             $(this.elem).append(derezElem);

ChromeExt/src/contentscript/BackpackWindow.ts
@@ -157,9 +157,8 @@ export class BackpackWindow extends Window
                             if (roomItem) {
                                 const x = Math.round(ui.offset.left - $(paneElem).offset().left + ui.draggable.width() / 2);
                                 const y = Math.round(ui.offset.top - $(paneElem).offset().top + ui.draggable.height() / 2)
-                                roomItem.beginDerez();
-                                await this.derezItem(roomItem.getRoomNick(), roomItem.getRoom().getJid(), x, y);
-                                roomItem.endDerez();
+                                const itemId = roomItem.getProperties()[Pid.Id];
+                                this.app.derezItem(itemId, x, y);
                             }
                         }
                     }
@@ -271,18 +270,6 @@ export class BackpackWindow extends Window
             }
 
             await BackgroundMessage.rezBackpackItem(itemId, room, x, destination, {});
-        } catch (ex) {
-            new ItemExceptionToast(this.app, Config.get('room.errorToastDurationSec', 8), ex).show();
-        }
-    }
-
-    derezItemSync(itemId: string, room: string, x: number, y: number) { this.derezItem(itemId, room, x, y); }
-    async derezItem(itemId: string, room: string, x: number, y: number)
-    {
-        if (Utils.logChannel('backpackWindow', true)) { log.info('BackpackWindow.derezItem', itemId, 'from', room); }
-        try {
-            await BackgroundMessage.derezBackpackItem(itemId, room, -1, -1, {}, [Pid.AutorezIsActive], {});
-
         } catch (ex) {
             new ItemExceptionToast(this.app, Config.get('room.errorToastDurationSec', 8), ex).show();
         }

ChromeExt/src/contentscript/Participant.ts
@@ -1050,14 +1050,7 @@ export class Participant extends Entity
                     'Derezzing item...',
                     'roomItem', roomItem, 'roomJid', roomJid);
             }
-            BackgroundMessage.derezBackpackItem(
-                itemId, roomJid, -1, -1,
-                {}, [Pid.AutorezIsActive], {}
-            ).catch(error => { this.app.onError(
-                'Participant.applyItem',
-                'BackgroundMessage.derezBackpackItem failed!',
-                error, 'this', this, 'roomItem', roomItem);
-            });
+            this.app.derezItem(itemId);
         } else {
             if (Utils.logChannel('items', true)) {
                 log.info('Participant.applyItem',

ChromeExt/src/contentscript/RoomItem.ts
@@ -199,7 +199,7 @@ export class RoomItem extends Entity
                             // The new item is better
                             if (await BackgroundMessage.isBackpackItem(claimingRoomItem.getRoomNick())) {
                                 // The existing claim is mine
-                                await BackgroundMessage.derezBackpackItem(claimingRoomItem.getRoomNick(), this.room.getJid(), -1, -1, {}, [Pid.AutorezIsActive], {});
+                                this.app.derezItem(claimingRoomItem.getRoomNick());
                                 new SimpleToast(this.app, 'ClaimDerezzed', Config.get('room.claimToastDurationSec', 15), 'notice', this.app.translateText('Toast.Your claim has been removed'), 'A stronger item just appeared').show();
                             }
                         }
@@ -303,11 +303,7 @@ export class RoomItem extends Entity
                         } catch (error) {
                             // Ignore
                         }
-                        try {
-                            await BackgroundMessage.derezBackpackItem(itemId, this.getRoom().getJid(), -1, -1, {}, [], {});
-                        } catch (error) {
-                            // Ignore
-                        }
+                        this.app.derezItem(itemId);
                     }, maxDuration * 1000);
                 }
             }
@@ -361,25 +357,53 @@ export class RoomItem extends Entity
     {
         super.onMouseClickAvatar(ev);
 
-        if (as.Bool(this.properties[Pid.IframeAspect])) {
-            const frame = as.String(JSON.parse(as.String(this.properties[Pid.IframeOptions], '{}')).frame, 'Window');
-            if (frame === 'Popup') {
-                if (this.framePopup) {
-                    this.getScriptWindow()?.postMessage({ [Config.get('iframeApi.messageMagicRezactive', 'tr67rftghg_Rezactive')]: true, type: 'Window.Close' }, '*');
-                    window.setTimeout(() => { this.framePopup.close(); }, 100);
-                } else {
-                    this.openFrame(this.getElem());
-                }
-            } else {
-                if (this.frameWindow) {
-                    if (this.frameWindow.isOpen()) {
-                        this.frameWindow.setVisibility(true);
-                        this.frameWindow.toFront();
+        if (!ev.shiftKey && !ev.ctrlKey && !ev.altKey && !ev.metaKey) {
+            // Click without modifier key.
+            if (as.Bool(this.properties[Pid.IframeAspect], false)) {
+                // Open item dialog:
+                const frameOptsStr = this.properties[Pid.IframeOptions] ?? '{}';
+                const frameOpts = JSON.parse(frameOptsStr);
+                const frame = frameOpts.frame ?? 'Window';
+                let openFrame = false;
+                if (frame === 'Popup') {
+                    if (this.framePopup) {
+                        const magicKey = Config.get(
+                            'iframeApi.messageMagicRezactive',
+                            'tr67rftghg_Rezactive');
+                        const msg = {[magicKey]: true, type: 'Window.Close'};
+                        this.getScriptWindow()?.postMessage(msg, '*');
+                        window.setTimeout((): void => {
+                            this.framePopup.close();
+                        }, 100);
+                    } else {
+                        openFrame = true;
                     }
                 } else {
-                    this.openFrame(this.getElem());
+                    if (this.frameWindow) {
+                        if (this.frameWindow.isOpen()) {
+                            this.frameWindow.setVisibility(true);
+                            this.frameWindow.toFront();
+                        }
+                    } else {
+                        openFrame = true;
+                    }
+                }
+                if (openFrame) {
+                    this.openFrame(this.getElem()
+                    ).catch(error => { this.app.onError(
+                        'RoomItem.onMouseClickAvatar',
+                        'this.openFrame failed!',
+                        error, 'this', this,
+                    )});
                 }
             }
+        } else if (!ev.shiftKey && ev.ctrlKey && !ev.altKey && !ev.metaKey) {
+            // CTRL + click.
+            if (this.myItem) {
+                this.app.derezItem(this.properties[Pid.Id]);
+            }
+        } else {
+            // Other clicks.
         }
 
         this.statsDisplay?.close();

ChromeExt/src/lib/_Changes.ts
@@ -7,6 +7,7 @@ export class _Changes
 {
     static data: IHistory = [
         ['1.1.4', 'Effects', [
+            ['Add', 'CTRL+click drops/pickups item'],
             ['Add', 'Confirmation on item transfer'],
             ['Add', 'Create NFT avatar failure toast with toggle backpack link'],
             ['Add', 'Support page effects'],

commit 6c9806476f8921311fc73557c7ea1d75c327faa9

Fix regression deleting item state on derezz (#27).
- Regression introduced in commit 154896b0d7e0e219efa487a495e0fae98a51a730.

ChromeExt/src/contentscript/ContentApp.ts
@@ -1080,7 +1080,7 @@ export class ContentApp
         const props = await BackgroundMessage.getBackpackItemProperties(itemId);
         const roomJid = props[Pid.RezzedLocation];
         const [x, y] = [xNew ?? -1, yNew ?? -1];
-        const propsDel = [Pid.AutorezIsActive, Pid.State];
+        const propsDel = [Pid.AutorezIsActive];
         if (Utils.logChannel('items')) {
             log.info('ContentApp.derezItemAsync', 'itemId', itemId, 'roomJid', roomJid);
         }