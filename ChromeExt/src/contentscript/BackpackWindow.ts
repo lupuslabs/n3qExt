lupuslabs/n3qExt

commit d297f6a046866531abab20a79dd1695e191f157a

Add delete button to backpack item info box (#18).

ChromeExt/src/contentscript/BackpackWindow.ts
@@ -12,7 +12,7 @@ import { Window, WindowOptions } from './Window';
 import { BackpackItem as BackpackItem } from './BackpackItem';
 import { Environment } from '../lib/Environment';
 import { ItemException } from '../lib/ItemException';
-import { ItemExceptionToast, SimpleToast } from './Toast';
+import { ItemExceptionToast } from './Toast';
 import { Avatar } from './Avatar';
 import { FreeSpace } from './FreeSpace';
 
@@ -46,138 +46,129 @@ export class BackpackWindow extends Window
         const width = as.Int(options.width, 600);
         let height = as.Int(options.height, 400);
 
-        if (this.windowElem) {
-            const windowElem = this.windowElem;
-            const contentElem = this.contentElem;
-            $(windowElem).addClass('n3q-backpackwindow');
+        const windowElem = this.windowElem;
+        const contentElem = this.contentElem;
+        $(windowElem).addClass('n3q-backpackwindow');
 
-            let left = as.Int(options.left, 50);
-            if (options.left == null) {
-                if (aboveElem) {
-                    left = Math.max(aboveElem.offsetLeft - 120, left);
-                }
+        let left = as.Int(options.left, 50);
+        if (options.left == null) {
+            if (aboveElem) {
+                left = Math.max(aboveElem.offsetLeft - 120, left);
             }
-            let top = this.app.getDisplay().offsetHeight - height - bottom;
+        }
+        let top = this.app.getDisplay().offsetHeight - height - bottom;
+        {
+            const minTop = 10;
+            if (top < minTop) {
+                height -= minTop - top;
+                top = minTop;
+            }
+        }
+
+        const paneElem = <HTMLElement>$('<div class="n3q-base n3q-backpack-pane" data-translate="children" />').get(0);
+        $(contentElem).append(paneElem);
+
+        const dumpElem = <HTMLElement>$('<div class="n3q-base n3q-backpack-dump" title="Shredder" data-translate="attr:title:Backpack"/>').get(0);
+        $(contentElem).append(dumpElem);
+        $(dumpElem).droppable({
+            tolerance: 'pointer',
+            drop: (ev: JQueryEventObject, ui: JQueryUI.DroppableEventUIParam) =>
             {
-                const minTop = 10;
-                if (top < minTop) {
-                    height -= minTop - top;
-                    top = minTop;
+                const droppedElem = ui.draggable.get(0);
+                if (droppedElem) {
+                    const droppedId: string = $(droppedElem).data('id');
+                    if (droppedId) {
+                        const onCancel = () => this.itemVisibility(droppedId, true);
+                        this.app.deleteItemAsk(droppedId, undefined, onCancel);
+                        this.itemVisibility(droppedId, false);
+                        ev.stopPropagation();
+                    }
                 }
             }
+        });
 
-            const paneElem = <HTMLElement>$('<div class="n3q-base n3q-backpack-pane" data-translate="children" />').get(0);
-            $(contentElem).append(paneElem);
+        if (Environment.isDevelopment()) {
+            const inElem = <HTMLElement>$('<textarea class="n3q-base n3q-backpack-in n3q-input n3q-text" />').get(0);
+            $(inElem).hide();
+            $(contentElem).append(inElem);
 
-            const dumpElem = <HTMLElement>$('<div class="n3q-base n3q-backpack-dump" title="Shredder" data-translate="attr:title:Backpack"/>').get(0);
-            $(contentElem).append(dumpElem);
-            $(dumpElem).droppable({
-                tolerance: 'pointer',
-                drop: async (ev: JQueryEventObject, ui: JQueryUI.DroppableEventUIParam) =>
-                {
-                    const droppedElem = ui.draggable.get(0);
-                    if (droppedElem) {
-                        const droppedId: string = $(droppedElem).data('id');
-                        if (droppedId) {
-
-                            const props = await BackgroundMessage.getBackpackItemProperties(droppedId);
-                            const itemName = props[Pid.Label] ?? props[Pid.Template];
-                            const toast = new SimpleToast(this.app, 'backpack-reallyDelete', as.Float(Config.get('backpack.deleteToastDurationSec'), 1000), 'question', 'Really delete?', this.app.translateText('ItemLabel.' + itemName) + '\n' + droppedId);
-                            toast.actionButton('Yes, delete item', () => { this.deleteItem(droppedId); toast.close(); });
-                            toast.actionButton('No, keep it', () => { this.itemVisibility(droppedId, true); toast.close(); });
-                            toast.setDontShow(false);
-                            toast.show(() => { this.itemVisibility(droppedId, true); });
-                            this.itemVisibility(droppedId, false);
-
-                            ev.stopPropagation();
-                        }
-                    }
+            const toggleElem = <HTMLElement>$('<div class="n3q-base n3q-absolutebutton n3q-backpack-toggle">Input</div>').get(0);
+            $(contentElem).append(toggleElem);
+            $(toggleElem).on('click', () =>
+            {
+                if ($(inElem).is(':hidden')) {
+                    $(inElem).show();
+                    this.app.toFront(inElem, ContentApp.LayerWindowContent);
+                } else {
+                    $(inElem).hide();
                 }
             });
 
-            if (Environment.isDevelopment()) {
-                const inElem = <HTMLElement>$('<textarea class="n3q-base n3q-backpack-in n3q-input n3q-text" />').get(0);
-                $(inElem).hide();
-                $(contentElem).append(inElem);
-
-                const toggleElem = <HTMLElement>$('<div class="n3q-base n3q-absolutebutton n3q-backpack-toggle">Input</div>').get(0);
-                $(contentElem).append(toggleElem);
-                $(toggleElem).on('click', () =>
-                {
-                    if ($(inElem).is(':hidden')) {
-                        $(inElem).show();
-                        this.app.toFront(inElem, ContentApp.LayerWindowContent);
-                    } else {
-                        $(inElem).hide();
-                    }
-                });
-
-                const addElem = <HTMLElement>$('<div class="n3q-base n3q-absolutebutton n3q-backpack-add">Add</div>').get(0);
-                $(contentElem).append(addElem);
-                $(addElem).on('click', () =>
-                {
-                    let text = as.String($(inElem).val());
-                    text = text.replace(/'/g, '"');
-                    if (text !== '') {
-                        const json = JSON.parse(text);
-                        const itemId = Utils.randomString(30);
-                        json.Id = itemId;
-                        this.createItem(itemId, json, {});
-                    }
-                });
-            }
-
-            this.app.translateElem(windowElem);
-
-            $(windowElem).css({ 'width': width + 'px', 'height': height + 'px', 'left': left + 'px', 'top': top + 'px' });
-
-            this.onResizeStop = (ev: JQueryEventObject, ui: JQueryUI.ResizableUIParams) =>
+            const addElem = <HTMLElement>$('<div class="n3q-base n3q-absolutebutton n3q-backpack-add">Add</div>').get(0);
+            $(contentElem).append(addElem);
+            $(addElem).on('click', () =>
             {
-                const left = ui.position.left;
-                const bottom = this.app.getDisplay().offsetHeight - (ui.position.top + ui.size.height);
-                this.saveCoordinates(left, bottom, ui.size.width, ui.size.height);
-            };
+                let text = as.String($(inElem).val());
+                text = text.replace(/'/g, '"');
+                if (text !== '') {
+                    const json = JSON.parse(text);
+                    const itemId = Utils.randomString(30);
+                    json.Id = itemId;
+                    this.createItem(itemId, json, {});
+                }
+            });
+        }
 
-            this.onDragStop = (ev: JQueryEventObject, ui: JQueryUI.DraggableEventUIParams) =>
+        this.app.translateElem(windowElem);
+
+        $(windowElem).css({ 'width': width + 'px', 'height': height + 'px', 'left': left + 'px', 'top': top + 'px' });
+
+        this.onResizeStop = (ev: JQueryEventObject, ui: JQueryUI.ResizableUIParams) =>
+        {
+            const left = ui.position.left;
+            const bottom = this.app.getDisplay().offsetHeight - (ui.position.top + ui.size.height);
+            this.saveCoordinates(left, bottom, ui.size.width, ui.size.height);
+        };
+
+        this.onDragStop = (ev: JQueryEventObject, ui: JQueryUI.DraggableEventUIParams) =>
+        {
+            const size = { width: $(this.windowElem).width(), height: $(this.windowElem).height() };
+            const left = ui.position.left;
+            const bottom = this.app.getDisplay().offsetHeight - (ui.position.top + size.height);
+            this.saveCoordinates(left, bottom, size.width, size.height);
+        };
+
+        $(paneElem).droppable({
+            drop: async (ev: JQueryEventObject, ui: JQueryUI.DroppableEventUIParam) =>
             {
-                const size = { width: $(this.windowElem).width(), height: $(this.windowElem).height() };
-                const left = ui.position.left;
-                const bottom = this.app.getDisplay().offsetHeight - (ui.position.top + size.height);
-                this.saveCoordinates(left, bottom, size.width, size.height);
-            };
-
-            $(paneElem).droppable({
-                drop: async (ev: JQueryEventObject, ui: JQueryUI.DroppableEventUIParam) =>
-                {
-                    const droppedElem = ui.draggable.get(0);
-                    if (droppedElem) {
-                        const droppedId = Avatar.getEntityIdByAvatarElem(droppedElem);
-                        if (droppedId) {
-                            const roomItem = this.app.getRoom().getItem(droppedId);
-                            if (roomItem) {
-                                const x = Math.round(ui.offset.left - $(paneElem).offset().left + ui.draggable.width() / 2);
-                                const y = Math.round(ui.offset.top - $(paneElem).offset().top + ui.draggable.height() / 2);
-                                const itemId = roomItem.getProperties()[Pid.Id];
-                                this.app.derezItem(itemId, x, y);
-                            }
+                const droppedElem = ui.draggable.get(0);
+                if (droppedElem) {
+                    const droppedId = Avatar.getEntityIdByAvatarElem(droppedElem);
+                    if (droppedId) {
+                        const roomItem = this.app.getRoom().getItem(droppedId);
+                        if (roomItem) {
+                            const x = Math.round(ui.offset.left - $(paneElem).offset().left + ui.draggable.width() / 2);
+                            const y = Math.round(ui.offset.top - $(paneElem).offset().top + ui.draggable.height() / 2);
+                            const itemId = roomItem.getProperties()[Pid.Id];
+                            this.app.derezItem(itemId, x, y);
                         }
                     }
                 }
-            });
-
-            this.paneElem = paneElem;
-
-            try {
-                const response = await BackgroundMessage.getBackpackState();
-                if (response && response.ok) {
-                    this.populate(response.items);
-                }
-
-                // let pos = this.getFreeCoordinate();
-
-            } catch (ex) {
-                // Ignored.
             }
+        });
+
+        this.paneElem = paneElem;
+
+        try {
+            const response = await BackgroundMessage.getBackpackState();
+            if (response && response.ok) {
+                this.populate(response.items);
+            }
+
+            // let pos = this.getFreeCoordinate();
+
+        } catch (ex) {
+            // Ignored.
         }
     }
 
@@ -270,18 +261,6 @@ export class BackpackWindow extends Window
             }
 
             await BackgroundMessage.rezBackpackItem(itemId, room, x, destination, {});
-        } catch (ex) {
-            new ItemExceptionToast(this.app, as.Float(Config.get('room.errorToastDurationSec'), 8), ex).show();
-        }
-    }
-
-    async deleteItem(itemId: string)
-    {
-        if (Utils.logChannel('backpackWindow', true)) { log.info('BackpackWindow.deleteItem', itemId); }
-        try {
-            await BackgroundMessage.deleteBackpackItem(itemId, { });
-            
-            this.app.getRoom().sendPresence();
         } catch (ex) {
             new ItemExceptionToast(this.app, as.Float(Config.get('room.errorToastDurationSec'), 8), ex).show();
         }

ChromeExt/src/contentscript/BackpackItemInfo.ts
@@ -172,6 +172,14 @@ export class BackpackItemInfo
                 $(this.elem).append(rezElem);
             }
         }
+        const delBtn = <HTMLElement>$('<div class="n3q-base n3q-button n3q-backpack-rez" data-translate="text:Backpack">Delete item</div>').get(0);
+        $(delBtn).on('click', (ev) =>
+        {
+            ev.stopPropagation();
+            this.app.deleteItemAsk(this.backpackItem.getItemId());
+            this.close();
+        });
+        $(this.elem).append(delBtn);
 
         if (Config.get('backpack.itemInfoExtended', false)) {
             this.extend();

ChromeExt/src/contentscript/ContentApp.ts
@@ -1089,4 +1089,52 @@ export class ContentApp
         );
     }
 
+    public deleteItemAsk(
+        itemId: string,
+        onDeleted?: (itemId: string) => void,
+        onCanceled?: (itemId: string) => void,
+        onFailed?: (itemId: string) => void, // For cleanups. Defaults to onCanceled.
+    ): void {
+        (async () => {
+            const props = await BackgroundMessage.getBackpackItemProperties(itemId);
+            const itemName = props[Pid.Label] ?? props[Pid.Template];
+            const duration = Config.get('backpack.deleteToastDurationSec', 1000);
+            const text = this.translateText('ItemLabel.' + itemName) + '\n' + itemId;
+            const toast = new SimpleToast(
+                this, 'backpack-reallyDelete', duration, 'question', 'Really delete?', text);
+            const onYes = () => {
+                toast.close();
+                this.deleteItem(itemId, onDeleted, onFailed ?? onCanceled);
+            };
+            const onNo = () => {
+                toast.close();
+                onCanceled?.(itemId);
+            };
+            toast.actionButton('Yes, delete item', onYes);
+            toast.actionButton('No, keep it', onNo);
+            toast.setDontShow(false);
+            toast.show(onNo);
+        })().catch(error => {this.onItemError(
+            'ContentApp.deleteItemAsk', 'Toast preparation failed!', error, 'itemId', itemId
+        )});
+    }
+
+    public deleteItem(
+        itemId: string,
+        onDeleted?: (itemId: string) => void,
+        onFailed?: (itemId: string) => void, // For cleanups.
+    ): void {
+        if (Utils.logChannel('items')) {
+            log.info('ContentApp.deleteItem', itemId);
+        }
+        (async () => {
+            await BackgroundMessage.deleteBackpackItem(itemId, {});
+            this.room?.sendPresence();
+            onDeleted?.(itemId);
+        })().catch(error => {
+            this.onItemError('ContentApp.deleteItem', 'Error caught!', error, 'itemId', itemId);
+            onFailed?.(itemId);
+        });
+    }
+
 }

ChromeExt/src/lib/Config.ts
@@ -379,6 +379,7 @@ export class Config
                     'Backpack.Go to item': 'Go there',
                     'Backpack.Derez item': 'Pick up',
                     'Backpack.Rez item': 'Drop',
+                    'Backpack.Delete item': 'Delete',
                     'Backpack.Enabled': 'Enabled',
                     'Backpack.Too many items': 'Too many items',
                     'Backpack.You are close to the limit of items on a page.': 'You are close to the limit of items on a page. All items will be hidden if the number rises above the limit.',
@@ -614,6 +615,7 @@ export class Config
                     'Backpack.Go to item': 'Dort hingehen',
                     'Backpack.Derez item': 'Einsammeln',
                     'Backpack.Rez item': 'Ablegen',
+                    'Backpack.Delete item': 'Löschen',
                     'Backpack.Enabled': 'Aktiv',
                     'Backpack.Too many items': 'Zu viele Gegenstände',
                     'Backpack.You are close to the limit of items on a page.': 'Du hast bald zu viele Gegenstände auf der Seite. Wenn die Grenze überschritten wird, werden alle Gegenstände ausgeblendet.',

commit 3d693a495c04ed704046df307dda1381123c2e76

Fix item not hidden on drag on shredder.
- Regression from commit d297f6a046866531abab20a79dd1695e191f157a (#18).
- What has gone wrong: The event handler was async before and therefore the item
  element was swapped back from the drag-'n-drop copy before the actual handler
  code was executed. This led to the hide call to affect the correct element.
  The newly introduced window.setTimeout has the same effect of delaying the
  hide operation to after the element swap has happened.

ChromeExt/src/contentscript/BackpackWindow.ts
@@ -80,7 +80,7 @@ export class BackpackWindow extends Window
                     if (droppedId) {
                         const onCancel = () => this.itemVisibility(droppedId, true);
                         this.app.deleteItemAsk(droppedId, undefined, onCancel);
-                        this.itemVisibility(droppedId, false);
+                        window.setTimeout(() => this.itemVisibility(droppedId, false), 1);
                         ev.stopPropagation();
                     }
                 }