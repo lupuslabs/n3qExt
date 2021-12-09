7109db1169ee3c59904da83144301d53b2184a55
Heiner Wolf <git@heinerwolf.de>
2021-12-09 23:29:15 +0100

    before deal with Rezzed

--- ChromeExt/src/background/HostedInventoryItemProvider.ts
+++ ChromeExt/src/background/HostedInventoryItemProvider.ts
@@ -43,7 +43,7 @@ export namespace HostedInventoryItemProvider
             let itemPropertySet = {};
             if (itemIds.length > 0) {
                 try {
-                    let request = new RpcProtocol.UserGetItemPropertiesRequest(await this.backpack.getUserId(), await this.backpack.getUserToken(), itemIds);
+                    const request = new RpcProtocol.UserGetItemPropertiesRequest(await this.backpack.getUserId(), await this.backpack.getUserToken(), itemIds);
                     const response = <RpcProtocol.UserGetItemPropertiesResponse>await this.rpcClient.call(this.config.apiUrl, request);
                     itemPropertySet = response.itemPropertySet;
                 } catch (ex) {
@@ -57,9 +57,9 @@ export namespace HostedInventoryItemProvider
 
             for (let itemId in itemPropertySet) {
                 const props = itemPropertySet[itemId];
-                let item = await this.backpack.createRepositoryItem(itemId, props);
+                const item = await this.backpack.createRepositoryItem(itemId, props);
                 if (item.isRezzed()) {
-                    let roomJid = item.getProperties()[Pid.RezzedLocation];
+                    const roomJid = item.getProperties()[Pid.RezzedLocation];
                     if (roomJid) {
                         this.backpack.addToRoom(itemId, roomJid);
                     }
@@ -79,14 +79,73 @@ export namespace HostedInventoryItemProvider
         {
             try {
                 let request = new RpcProtocol.UserItemActionRequest(
-                    await this.backpack.getUserId(), 
+                    await this.backpack.getUserId(),
                     await this.backpack.getUserToken(),
                     itemId,
                     action,
                     args,
                     involvedIds
-                    );
+                );
                 const response = <RpcProtocol.UserItemActionResponse>await this.rpcClient.call(this.config.apiUrl, request);
+
+                let changedOrCreated = [];
+                for (let i = 0; i < response.changedIds.length; i++) {
+                    const id = response.changedIds[i];
+                    if (!changedOrCreated.includes(id)) {
+                        changedOrCreated.push(id);
+                    }
+                }
+                for (let i = 0; i < response.createdIds.length; i++) {
+                    const id = response.createdIds[i];
+                    if (!changedOrCreated.includes(id)) {
+                        changedOrCreated.push(id);
+                    }
+                }
+
+                let itemPropertySet = {};
+                if (changedOrCreated.length > 0) {
+                    try {
+                        const request = new RpcProtocol.UserGetItemPropertiesRequest(await this.backpack.getUserId(), await this.backpack.getUserToken(), changedOrCreated);
+                        const response = <RpcProtocol.UserGetItemPropertiesResponse>await this.rpcClient.call(this.config.apiUrl, request);
+                        itemPropertySet = response.itemPropertySet;
+                    } catch (ex) {
+                        if (ex.fact) {
+                            throw new ItemException(ItemException.factFrom(ex.fact), ItemException.reasonFrom(ex.reason), ex.detail);
+                        } else {
+                            throw new ItemException(ItemException.Fact.NoItemsReceived, ItemException.Reason.NetworkProblem, as.String(ex.message, as.String(ex.status, '')));
+                        }
+                    }
+                }
+
+                if (response.createdIds) {
+                    for (let i = 0; i < response.createdIds.length; i++) {
+                        const id = response.createdIds[i];
+                        const props = itemPropertySet[id];
+                        let item = await this.backpack.createRepositoryItem(id, props);
+                        if (item.isRezzed()) {
+                            let roomJid = item.getProperties()[Pid.RezzedLocation];
+                            if (roomJid) {
+                                this.backpack.addToRoom(id, roomJid);
+                            }
+                        }
+                    }
+                }
+
+                if (response.changedIds) {
+                    for (let i = 0; i < response.changedIds.length; i++) {
+                        const id = response.changedIds[i];
+                        const props = itemPropertySet[id];
+                        await this.backpack.setItemProperties(id, props, {});
+                    }
+                }
+
+                if (response.deletedIds) {
+                    for (let i = 0; i < response.deletedIds.length; i++) {
+                        const id = response.deletedIds[i];
+                        await this.backpack.deleteItem(id, {});
+                    }
+                }
+
             } catch (ex) {
                 if (ex.fact) {
                     throw new ItemException(ItemException.factFrom(ex.fact), ItemException.reasonFrom(ex.reason), ex.detail);
