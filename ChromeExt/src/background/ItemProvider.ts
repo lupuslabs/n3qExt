0c99b9bd5005dd15c2aafaba1ba69eedf6565c0c
Heiner Wolf <git@heinerwolf.de>
2022-01-27 23:12:59 +0100

    ItemProvider.stanzaOutFilter for nickname to server

--- ChromeExt/src/background/ItemProvider.ts
+++ ChromeExt/src/background/ItemProvider.ts
@@ -14,12 +14,13 @@ export interface IItemProvider
     itemAction(itemId: string, action: string, args: any, involvedIds: string[], allowUnrezzed: boolean): Promise<ItemProperties>;
     rezItem(itemId: string, roomJid: string, rezzedX: number, destinationUrl: string, options: ItemChangeOptions): Promise<void>;
     derezItem(itemId: string, roomJid: string, inventoryX: number, inventoryY: number, changed: ItemProperties, deleted: Array<string>, options: ItemChangeOptions): Promise<void>;
-    getDependentPresence(itemId: string, roomJid: string): xml;
-    onDependentPresence(itemId: string, roomJid: string, participantNick: string, dependentPresence: xml): void;
     loadWeb3Items(): Promise<void>;
     applyItemToItem(activeId: string, passiveId: string): Promise<void>;
     createItem(auth: string, method: string, args: ItemProperties): Promise<ItemProperties>;
     transferAuthorize(itemId: string, duration: number): Promise<string>;
     transferUnauthorize(itemId: string): Promise<void>;
     transferComplete(senderInventoryId: string, senderItemId: string, transferToken: string): Promise<string>;
+    stanzaOutFilter(stanza: xml): xml
+    getDependentPresence(itemId: string, roomJid: string): xml;
+    onDependentPresence(itemId: string, roomJid: string, participantNick: string, dependentPresence: xml): void;
 }
