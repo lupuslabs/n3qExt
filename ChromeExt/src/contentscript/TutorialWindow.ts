d6fbb6f1c804b9be220b589bff33bf1872f7d235
Heiner Wolf <git@heinerwolf.de>
2023-04-21 21:00:38 +0200

    tutorial: failed to auto forward after video ended

--- ChromeExt/src/contentscript/TutorialWindow.ts
+++ ChromeExt/src/contentscript/TutorialWindow.ts
@@ -3,6 +3,15 @@ import { ContentApp } from './ContentApp';
 import { domHtmlElemOfHtml } from '../lib/domTools'
 import { PointerEventDispatcher } from '../lib/PointerEventDispatcher'
 import { Config } from '../lib/Config';
+import { as } from '../lib/as';
+
+// declare global
+// {
+//     interface Window
+//     {
+//         YT: any;
+//     }
+// }
 
 interface Video
 {
@@ -42,8 +51,8 @@ export class TutorialWindow extends Window<WindowOptions> {
         super.prepareMakeDom();
         this.windowCssClasses.push('n3q-tutorialwindow');
         this.titleText = this.app.translateText('TutorialWindow.Tutorial', 'Tutorial');
-        this.defaultWidth = Config.get('tutorial.defaultWidth', 800);
-        this.defaultHeight = Config.get('tutorial.defaultHeight', 600);
+        this.defaultWidth = Config.get('tutorial.defaultWidth', 1040);
+        this.defaultHeight = Config.get('tutorial.defaultHeight', 665);
         this.defaultBottom = Config.get('tutorial.defaultBottom', 400);
         this.defaultLeft = Config.get('tutorial.defaultLeft', 50);
     }
@@ -53,7 +62,7 @@ export class TutorialWindow extends Window<WindowOptions> {
         await super.makeContent();
         const contentElem = this.contentElem;
 
-        const pane = domHtmlElemOfHtml('<div class="n3q-base n3q-tutorialwindow-pane" data-translate="children"></div>');
+        const pane = domHtmlElemOfHtml('<div class="n3q-base n3q-tutorialwindow-pane" data-translate="children"></script></div>');
 
         this.videoTitle = domHtmlElemOfHtml('<div class="n3q-tutorialwindow-video-title"></div>');
         this.videoContainer = domHtmlElemOfHtml('<div class="n3q-tutorialwindow-video-container"></div>');
@@ -80,7 +89,7 @@ export class TutorialWindow extends Window<WindowOptions> {
 
         this.videos.forEach((elem, index) =>
         {
-            const dot = domHtmlElemOfHtml('<div class="n3q-tutorialwindow-dot" data-index="' + index + '" title="' + elem.title + '"></div>');
+            const dot = domHtmlElemOfHtml('<div class="n3q-tutorialwindow-dot" data-index="' + index + '" title="' + as.Html(elem.title) + '"></div>');
             PointerEventDispatcher.makeOpaqueDispatcher(this.app, dot).addUnmodifiedLeftClickListener(ev => { this.onDotClick(index); });
             this.dotsContainer.appendChild(dot);
         });
@@ -91,7 +100,7 @@ export class TutorialWindow extends Window<WindowOptions> {
     private updateVideo(): void
     {
         this.videoTitle.textContent = this.videos[this.currentVideoIndex].title;
-        this.videoContainer.innerHTML = `<iframe src="${this.videos[this.currentVideoIndex].url.replace('youtu.be', 'youtube.com/embed')}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+        this.videoContainer.innerHTML = `<iframe src="${this.videos[this.currentVideoIndex].url.replace('youtu.be', 'youtube.com/embed')}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
 
         this.videos.forEach((elem, index) =>
         {
@@ -103,6 +112,57 @@ export class TutorialWindow extends Window<WindowOptions> {
             }
         });
     }
+    // private updateVideo(): void
+    // {
+    //     this.videoTitle.textContent = this.videos[this.currentVideoIndex].title;
+    //     this.videoContainer.innerHTML = `<div id="player"></div>`;
+
+    //     this.videos.forEach((elem, index) =>
+    //     {
+    //         const dot = this.dotsContainer.querySelector('[data-index="' + index + '"]');
+    //         if (index == this.currentVideoIndex) {
+    //             dot.classList.add('n3q-active');
+    //         } else {
+    //             dot.classList.remove('n3q-active');
+    //         }
+    //     });
+
+    //     // Load the YouTube player API script
+    //     const script = document.createElement('script');
+    //     script.src = 'https://www.youtube.com/player_api';
+    //     this.videoContainer.appendChild(script);
+
+    //     // Initialize the player when the API script has finished loading
+    //     script.onload = () =>
+    //     {
+    //         const player = new YT.Player('player', {
+    //             videoId: this.videos[this.currentVideoIndex].url.split('/').pop(),
+    //             playerVars: {
+    //                 autoplay: 1,
+    //                 controls: 1,
+    //                 disablekb: 1,
+    //                 enablejsapi: 1,
+    //                 modestbranding: 1,
+    //                 rel: 0,
+    //                 showinfo: 0
+    //             },
+    //             events: {
+    //                 'onStateChange': (event: YT.OnStateChangeEvent) =>
+    //                 {
+    //                     if (event.data == YT.PlayerState.ENDED) {
+    //                         setTimeout(() =>
+    //                         {
+    //                             if (this.currentVideoIndex < this.videos.length - 1) {
+    //                                 this.currentVideoIndex++;
+    //                                 this.updateVideo();
+    //                             }
+    //                         }, 3000);
+    //                     }
+    //                 }
+    //             }
+    //         });
+    //     };
+    // }
 
     private onPreviousClick(): void
     {
