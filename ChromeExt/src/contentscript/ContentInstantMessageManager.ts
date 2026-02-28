d0e827b20726120437421614c0d0b726045cd2a4
Allan Wegan <allanwegan@allanwegan.de>
2026-02-28 06:59:02 +0100

    Fix private chat feature opening different chatrooms for both users if usernames out of sync
    This happened when a user got a changed username due to an XMPP reconnect while the old connection was still timing out.

--- ChromeExt/src/contentscript/ContentInstantMessageManager.ts
+++ ChromeExt/src/contentscript/ContentInstantMessageManager.ts
@@ -183,8 +183,8 @@ export class ContentInstantMessageManager
             }
 
             const vidconfId = await this.getPrivateVidconfId(otherUserInfo)
-            const usersText = [this.app.getUserName(), otherUserInfo.userName].sort().join('-')
-            const vidconfRoomId = `-private-${usersText}-${vidconfId}`
+            const userIdsText = [this.app.getUserId(), otherUserInfo.userId].sort().join('-')
+            const vidconfRoomId = `-private-${userIdsText}-${vidconfId}`
             const urlTemplate = as.String(Config.get('room.vidconfUrl'), 'https://video.weblin.io/Vidconf?room=weblin{room}&name={name}');
             const url = urlTemplate
                 .replace('{room}', encodeURIComponent(vidconfRoomId))
